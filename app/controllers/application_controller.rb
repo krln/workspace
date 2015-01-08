1dfc8c7528689c96f65675df9fe37de902b4e4ab
krln <krlnkmsk88@gmail.com>
2015-01-08 11:17:16 +0100

    changes in app_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,4 @@
 class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
-  def hello
-      render text: "¡Hola, mundo!"
-  end
-
-#  def goodbye
-#      render text: "goodbye, world!"
-#  end
 end
