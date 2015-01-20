8f7c788f452b02d316899f5f74e132cd1ed67555
krln <krlnkmsk88@gmail.com>
2015-01-20 10:15:45 +0100

    done microposts

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -13,13 +13,21 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
       post microposts_path, micropost: { content: "" }
     end
     assert_select 'div#error_explanation'
-
     content = "This micropost really ties the room together"
     assert_difference 'Micropost.count', 1 do
       post microposts_path, micropost: { content: content }
     end
+
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
-  end
+
+    first_micropost = @user.microposts.paginate(page: 1).first
+    assert_difference 'Micropost.count', -1 do
+    delete micropost_path(first_micropost)
+    end
+    get user_path(users(:archer))
+    assert_select 'a', text: 'delete', count: 0
+    end
+
 end
